cbadb444786cf8f693f9336265e4299509d38d2f
meishenlieshou <meishenlieshou@gmail.com>
2025-01-18 19:35:48 +0800

    更新提交

--- docs/.vuepress/.temp/pages/learn/vite/L6uVy/index.html.js
+++ docs/.vuepress/.temp/pages/learn/vite/L6uVy/index.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/Administrator/Desktop/workspace/note/docs/.vuepress/.temp/pages/learn/vite/L6uVy/index.html.vue"
-const data = JSON.parse("{\"path\":\"/learn/vite/L6uVy/\",\"title\":\"Vite\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"Vite\",\"createTime\":\"2025/01/18 07:21:14\",\"permalink\":\"/learn/vite/L6uVy/\",\"author\":\"JackSim\",\"tags\":[\"Vite\",\"前端工程化\"],\"description\":null,\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":0.06,\"words\":19},\"filePathRelative\":\"notes/学习/Vue/Vite.md\",\"bulletin\":false}")
+const data = JSON.parse("{\"path\":\"/learn/vite/L6uVy/\",\"title\":\"Vite\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"Vite\",\"createTime\":\"2025/01/18 07:21:14\",\"permalink\":\"/learn/vite/L6uVy/\",\"author\":\"JackSim\",\"tags\":[\"Vite\",\"前端工程化\"],\"description\":null,\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":4.29,\"words\":1287},\"filePathRelative\":\"notes/学习/Vue/Vite.md\",\"bulletin\":false}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
